03293aabc12ac3729caf2127bfe2c2f79ecd3221
David Losada <david@tuxpiper.com>
2016-07-04 12:48:13 +0200

    v3.4.5

--- build_env.sh
+++ build_env.sh
@@ -1,9 +1,9 @@
-client_version=v3.2.1-release.1
-api_version=v3.2.1-release.1
-release_version=${CI_BRANCH:-v3.2.1-release.1}
+client_version=v3.4.5
+api_version=v3.4.5
+release_version=${CI_BRANCH:-v3.4.5}
 
 client_url=
 api_url=
 
-client_tar=ushahidi-platform-client-bundle-${client_version}.tgz
-api_tar=ushahidi-platform-bundle-${api_version}.tgz
+client_tar=ushahidi-platform-client-bundle-${client_version}.tar.gz
+api_tar=ushahidi-platform-bundle-${api_version}.tar.gz
