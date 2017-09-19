7fdbdd0500b702ba6a38e4c4942410c45d38be9b
Romina Suarez <rowasc@gmail.com>
2017-09-19 12:49:55 -0300

    v3.7.2-rc.2

--- build_env.sh
+++ build_env.sh
@@ -1,6 +1,6 @@
-client_version=v3.7.2-rc.1
-api_version=v3.7.2-rc.1
-release_version=${CI_BRANCH:-v3.7.2-rc.1}
+client_version=v3.7.2-rc.2
+api_version=v3.7.2-rc.2
+release_version=${CI_BRANCH:-v3.7.2-rc.2}
 
 client_url=
 api_url=
