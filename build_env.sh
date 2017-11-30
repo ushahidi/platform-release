1c585373358b7f9c99b388f5c77eef7555a56450
David Losada <david@tuxpiper.com>
2017-11-30 10:40:14 +0100

    v3.8.0

--- build_env.sh
+++ build_env.sh
@@ -1,6 +1,6 @@
-client_version=v3.7.2-rc.2
-api_version=v3.7.2-rc.2
-release_version=${CI_BRANCH:-v3.7.2-rc.2}
+client_version=v3.8.0
+api_version=v3.8.0
+release_version=${CI_BRANCH:-v3.8.0}
 
 client_url=
 api_url=
