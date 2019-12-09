5fc07eae6ee02879d50e8290d336f6002ffcdfd0
rowasc <rowasc@gmail.com>
2019-12-09 11:38:37 -0300

    Release v4.2.0

--- build_env.sh
+++ build_env.sh
@@ -1,6 +1,6 @@
-client_version=v4.1.0
-api_version=v4.1.0
-release_version=${CI_BRANCH:-v4.1.0}
+client_version=v4.2.0
+api_version=v4.2.0
+release_version=${CI_BRANCH:-v4.2.0}
 
 client_url=
 api_url=
