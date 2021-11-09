e1b7e860c26142d7d854fed0767e9326b2933553
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-09 16:04:49 +0800

    Delete build_env.sh

--- build_env.sh
+++
@@ -1,9 +0,0 @@
-client_version=v5.1.3
-api_version=v5.1.3
-release_version=${CI_BRANCH:-v5.1.3}
-
-client_url=
-api_url=
-
-client_tar=ushahidi-platform-client-bundle-${client_version}.tar.gz
-api_tar=ushahidi-platform-bundle-${api_version}.tar.gz
