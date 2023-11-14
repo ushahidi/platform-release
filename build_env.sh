005f685703fc634f549cf3401b63540f0dc6e4b6
David Losada Carballo <david@tuxpiper.com>
2023-11-14 12:40:08 +0100

    v6.0.0-rc.2 : critical fixes

--- build_env.sh
+++ build_env.sh
@@ -1,6 +1,6 @@
 client_version=v2023.42.1+001
-api_version=v2023.42.1
-release_version=${CI_BRANCH:-v6.0.0-rc.1}
+api_version=v2023.42.5
+release_version=${CI_BRANCH:-v6.0.0-rc.2}
 
 client_url=
 api_url=
