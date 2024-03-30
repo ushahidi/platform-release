47014578390cf072d352d79c21cdb9df15b1d3a2
David Losada Carballo <david@tuxpiper.com>
2024-03-30 11:13:43 +0100

    v6.0.5

--- build_env.sh
+++ build_env.sh
@@ -1,6 +1,6 @@
-client_version=v2024.10.0
-api_version=v2024.10.0
-release_version=${RELEASE_VERSION:-v6.0.4}
+client_version=v2024.12.0
+api_version=v2024.12.1
+release_version=${RELEASE_VERSION:-v6.0.5}
 
 client_url=
 api_url=
